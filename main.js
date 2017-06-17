8d1c5434e13d646cfbf1685c320974abcf155196
Maja Jakobsson <maja.jacobsson@gmail.com>
2017-06-17 15:24:52 +0200

    almost working

---
+++ main.js
@@ -0,0 +1,24 @@
+console.log('hej');
+
+var menuBar = document.getElementById("menuBar");
+var burgerMenu = document.getElementById("burgerMenu");
+var mainMessage = document.getElementById("mainMessage");
+
+if (document.body.clientWidth > 550) {
+  menuBar.classList.remove("is-hidden");
+  burgerMenu.classList.add("is-hidden");
+} else {
+  burgerMenu.classList.remove("is-hidden");
+  menuBar.classList.add("is-hidden");
+};
+
+window.addEventListener("resize", function(event){
+  if (document.body.clientWidth > 550) {
+    menuBar.classList.remove("is-hidden");
+    burgerMenu.classList.add("is-hidden");
+  } else {
+    burgerMenu.classList.remove("is-hidden");
+    menuBar.classList.add("is-hidden");
+  };
+
+});
