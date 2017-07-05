c31a0f2303061b4186ee1b3fff4be79725ba7971
Maja Jakobsson <maja.jacobsson@gmail.com>
2017-07-05 16:32:31 -0400

    opacity on image

--- catgame/engine.js
+++ catgame/engine.js
@@ -4,11 +4,14 @@ var south=0;
 var west=0;
 var east=0;
 var worldsize=3;
+var score=0;
 
 var northbutton = document.getElementById("northbutton");
 var westbutton = document.getElementById("westbutton");
 var eastbutton = document.getElementById("eastbutton");
 var southbutton = document.getElementById("southbutton");
+
+var myImage = document.getElementById("myImage");
 
 northbutton.addEventListener('click', function(event) {
   if (north > worldsize) {
@@ -16,8 +19,10 @@ northbutton.addEventListener('click', function(event) {
   }
   if (south < 1) {
     north = north + 1;
+    score = score + 1;
   } else {
     south = south - 1;
+    score = score + 1;
   }
   myPosition();
 });
@@ -28,8 +33,10 @@ westbutton.addEventListener('click', function(event) {
   }
   if (east < 1) {
     west = west + 1;
+    score = score + 1;
   } else {
     east = east - 1;
+    score = score + 1;
   }
   myPosition();
 });
@@ -40,8 +47,10 @@ eastbutton.addEventListener('click', function(event) {
   }
   if (west < 1) {
     east = east + 1;
+    score = score + 1;
   } else {
     west = west - 1;
+    score = score + 1;
   }
   myPosition();
 });
@@ -52,8 +61,10 @@ southbutton.addEventListener('click', function(event) {
   }
   if (north > 0) {
     north = north - 1;
-  } else{
+    score = score + 1;
+  } else {
   south = south + 1;
+  score = score + 1;
   }
   myPosition();
 });
@@ -62,6 +73,10 @@ function myPosition() {
   var position = [north, south, east, west];
   // console.log("North: " + north +"   South: " + south+"   East: " + east + "   West: " + west);
   console.log(position);
+  console.log(score);
+  if (score > 3) {
+    myImage.classList.remove("img--fifty");
+  }
 };
 
 })();
